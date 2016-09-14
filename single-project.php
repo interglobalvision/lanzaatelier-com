8a705f0df8c8017e206b49fd8ec8f4b6f6696420
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-14 11:03:48 -0500

    add single-project template

---
+++ single-project.php
@@ -0,0 +1,44 @@
+<?php
+get_header();
+?>
+
+<main id="main-content" class="margin-top-mid">
+  <section id="projects">
+
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+?>
+
+    <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+      <div class="container">
+        <div class="grid-row">
+
+          <div class="grid-item item-s-12 item-l-6">
+            <?php the_title(); ?>
+            <?php the_content(); ?>
+          </div>
+
+          <div class="grid-item item-s-12 item-l-6">
+            // SLIDER
+          </div>
+
+        </div>
+      </div>
+    </article>
+
+<?php
+  }
+} else {
+?>
+    <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+<?php
+} ?>
+  </section>
+
+</main>
+
+<?php
+get_footer();
+?>
