404342c2f0a83128a74cf85511a43c77f322c5f6
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-14 19:12:35 -0500

    add text, drawings, and photo gallery markup

--- single-project.php
+++ single-project.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 
-<main id="main-content" class="margin-top-mid">
+<main id="main-content">
   <section id="projects">
 
 <?php
@@ -15,22 +15,52 @@ if( have_posts() ) {
       <div class="container">
         <div class="grid-row">
 
-          <div class="grid-item item-s-12 item-l-6">
-            <h1 class="single-project-title"><?php the_title(); ?></h1>
-            <?php the_content(); ?>
+          <div class="grid-item item-s-12 item-l-6 project-content-holder">
+<?php
+    $drawings = get_post_meta($post->ID, '_igv_project_drawings', true);
+
+    if (!empty($drawings)) {
+?>
+            <div class="project-drawings">
+              <h1 class="padding-left-basic"><?php the_title(); ?></h1>
+<?php
+      foreach($drawings as $image) {
+        echo wp_get_attachment_image($image, 'item-l-6-4x3');
+      }
+?>
+            </div>
+<?php
+    }
+?>
+            <div class="project-text">
+              <h1 class="padding-left-basic"><?php the_title(); ?></h1>
+              <?php the_content(); ?>
+            </div>
           </div>
 
           <div class="grid-item item-s-12 item-l-6">
 <?php
-    $gallery = get_post_meta($post->ID, '_igv_project_gallery', true);
+    $photos = get_post_meta($post->ID, '_igv_project_photos', true);
 
-    if (!empty($gallery)) {
-      foreach($gallery as $image) {
-        echo wp_get_attachment_image($image, 'item-l-6-4x3');
+    if (!empty($photos)) {
+?>
+            <div class="swiper-container">
+              <div class="swiper-wrapper">
+<?php
+      foreach($photos as $image) {
+?>
+                <div class="swiper-slide grid-column justify-center align-items-center">
+                  <?php echo wp_get_attachment_image($image, 'item-l-6-4x3'); ?>
+                </div>
+<?php
       }
+?>
+              </div>
+            </div>
+<?php
     } 
 ?>
-
+          <div class="project-gallery-pagination padding-top-tiny padding-left-mid"></div>
           </div>
 
         </div>
