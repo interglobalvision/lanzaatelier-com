6f1eaa47fbfef54e88906da1cde10d10d2f8dfc2
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-16 14:12:35 -0500

    update index margins

--- index.php
+++ index.php
@@ -12,13 +12,13 @@ if( have_posts() ) {
     the_post();
 ?>
 
-        <article <?php post_class('grid-item item-s-12 item-m-6 item-l-4'); ?> id="post-<?php the_ID(); ?>">
+        <article <?php post_class('grid-item item-s-12 item-m-6 item-l-4 margin-bottom-small'); ?> id="post-<?php the_ID(); ?>">
 
           <?php the_post_thumbnail('item-l-4-3x4'); ?>
 
-          <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+          <h1 class="font-size-basic margin-top-tiny margin-bottom-tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
 
-          <?php the_content(); ?>
+          <div class="padding-left-small"><?php the_content(); ?></div>
 
         </article>
 
