48a49527fdd6ddd6c80294eeefcb7e83824cf5c7
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-16 12:12:54 -0500

    update index markup

--- index.php
+++ index.php
@@ -2,31 +2,35 @@
 get_header();
 ?>
 
-<main id="main-content" class="margin-top-large">
+<main id="main-content">
   <section id="posts">
-
+    <div class="container">
+      <div class="grid-row">
 <?php
 if( have_posts() ) {
   while( have_posts() ) {
     the_post();
 ?>
 
-    <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+        <article <?php post_class('grid-item item-s-12 item-m-6 item-l-4'); ?> id="post-<?php the_ID(); ?>">
 
-      <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+          <?php the_post_thumbnail(); ?>
 
-      <?php the_content(); ?>
+          <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
 
-    </article>
+          <?php the_content(); ?>
+
+        </article>
 
 <?php
   }
 } else {
 ?>
-    <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+        <article class="u-alert grid-item item-s-12"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
 <?php
 } ?>
-
+      </div>
+    </div>
   </section>
 
   <?php get_template_part('partials/pagination'); ?>
