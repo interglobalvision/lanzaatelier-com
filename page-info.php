0a1afbb5cc863a9b75c70e2921dbbcb21c6970dc
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-19 17:33:59 -0500

    fix grid mixin offset typo

---
+++ page-info.php
@@ -0,0 +1,41 @@
+<?php
+get_header();
+?>
+
+<main id="main-content" class="main-content-padding">
+  <section id="posts">
+    <div class="container">
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+?>
+
+        <article <?php post_class('grid-row'); ?> id="post-<?php the_ID(); ?>">
+          <div class="grid-item item-s-12 item-m-10 offset-m-1 item-l-6 offset-l-3">
+            <?php the_post_thumbnail('item-l-6-4x3'); ?>
+          </div>
+
+          <div class="grid-item item-s-12 item-m-12 item-l-8 offset-l-2">
+            <?php the_content(); ?>
+          </div>
+        </article>
+
+<?php
+  }
+} else {
+?>
+        <article class="u-alert grid-row">
+          <div class="grid-item item-s-12">
+            <?php _e('Sorry, page not found'); ?>
+          </div>
+        </article>
+<?php
+} ?>
+    </div>
+  </section>
+</main>
+
+<?php
+get_footer();
+?>
