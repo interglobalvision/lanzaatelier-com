d4786c7708152bfb20b246ec0d799f3be248b58e
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-19 17:10:27 -0500

    update with shared classes to match archive

--- single-project.php
+++ single-project.php
@@ -15,7 +15,7 @@ if( have_posts() ) {
       <div class="container">
         <div class="grid-row">
 
-          <div class="grid-item item-s-12 item-l-6 project-content-holder">
+          <div class="grid-item item-s-12 item-l-6 single-project-drawings project-content-holder">
             <h1 class="padding-left-basic"><?php the_title(); ?></h1>
 <?php
     $drawings = get_post_meta($post->ID, '_igv_project_drawings', true);
@@ -28,23 +28,23 @@ if( have_posts() ) {
 ?>
           </div>
           
-          <div class="grid-item item-s-12 item-l-6 project-content-holder hide">
+          <div class="grid-item item-s-12 item-l-6 single-project-text project-content-holder hide">
             <h1 class="padding-left-basic project-text-title"><?php the_title(); ?></h1>
             <?php the_content(); ?>
           </div>
 
-          <div class="grid-item item-s-12 item-l-6">
+          <div class="grid-item item-s-12 item-l-6 single-project-photos">
 <?php
     $photos = get_post_meta($post->ID, '_igv_project_photos', true);
 
     if (!empty($photos)) {
 ?>
-            <div class="swiper-container">
+            <div class="swiper-container project-photos-container">
               <div class="swiper-wrapper">
 <?php
       foreach($photos as $image) {
 ?>
-                <div class="swiper-slide u-pointer grid-column justify-center align-items-center">
+                <div class="swiper-slide project-photo-holder u-pointer grid-column justify-center align-items-center">
                   <?php echo wp_get_attachment_image($image, 'item-l-6-4x3'); ?>
                 </div>
 <?php
