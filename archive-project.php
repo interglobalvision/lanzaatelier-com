f6ff499992f97e6248ea0c3bef71d87acbd49a85
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-19 17:10:11 -0500

    get image from photo gallery for archive. update markup and classes

--- archive-project.php
+++ archive-project.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 
-<main id="main-content" class="margin-top-mid">
+<main id="main-content" class="main-content-padding">
   <section id="projects">
 
 <?php
@@ -15,33 +15,34 @@ if( have_posts() ) {
 
           <div class="grid-item item-s-12 item-l-6">
             <ul>
-
 <?php
   while( have_posts() ) {
     the_post();
 ?>
-
               <li><a class="archive-project-title" href="<?php the_permalink() ?>" data-id="<?php echo get_the_ID(); ?>"><?php the_title(); ?></a></li>
-
 <?php
   }
 ?>
             </ul>
           </div>
 
-          <div class="grid-item item-s-12 item-l-6">
-
+          <div class="grid-item item-s-12 item-l-6 grid-column justify-center align-items-center archive-project-photos">
 <?php
   while( have_posts() ) {
     the_post();
+    $photos = get_post_meta($post->ID, '_igv_project_photos', true);
+
+    if (!empty($photos)) {
 ?>
-
-            <?php the_post_thumbnail('item-l-6-4x3', array('class'=>'archive-project-image', 'data-id'=>get_the_ID())); ?>
-
+            <div class="project-photos-container archive-photos-container hide grid-row" data-id="<?php echo $post->ID; ?>">
+              <div class="project-photo-holder grid-column justify-center align-items-center">
+                <?php echo wp_get_attachment_image($photos[0], 'item-l-6-4x3'); ?>
+              </div>
+            </div>
 <?php
+    }
   }
 ?>
-
           </div>
 
         </div>
