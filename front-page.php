67ac71cbc210f18845c7f3633a9a58c052d95731
Patrick Best <patrickbest@patrickbest.com>
2016-11-10 18:55:45 -0600

    revert to WP_Query
    shows latest 5 if no meta value is set

--- front-page.php
+++ front-page.php
@@ -9,52 +9,68 @@ get_header();
         <div class="grid-row">
 <?php
 
-// WP_Query arguments
 $projects = get_post_meta(get_the_ID(), '_igv_front_projects', true);
 
-//pr($projects); die;
+if (!empty($projects[0])) {
+  $project_ids = array_map(array_map_filter_project_ids, $projects);
+
+  $args = array (
+    'post_type' => array( 'project' ),
+    'post__in' => $project_ids
+  );
+
+} else {
+  $args = array (
+    'post_type'  => array( 'project' ),
+    'posts_per_page' => '5',
+  );
+}
+
+// The Query
+$query = new WP_Query( $args );
 
 // The Loop
-if ($projects) {
+if ( $query->have_posts() ) {
 ?>
       <div class="grid-item item-s-12 item-l-6">
         <div class="scroll-col scroll-col-left text-align-center" data-side="left">
 <?php
-  foreach ($projects as $project) {
+  while ( $query->have_posts() ) {
+    $query->the_post();
 
-    $image_left = get_post_meta($project['id'], '_igv_front_image_left_id', true);
-    $image_right = get_post_meta($project['id'], '_igv_front_image_right_id', true);
+    $image_left = get_post_meta($post->ID, '_igv_front_image_left_id', true);
+    $image_right = get_post_meta($post->ID, '_igv_front_image_right_id', true);
 
     if (!empty($image_left)) {
       $check_filetype = wp_check_filetype(wp_get_attachment_url($image_left));
-      if ($check_filetype['ext'] == 'gif') { 
+      if ($check_filetype['ext'] == 'gif') {
         $img_elem = '<img src="' . wp_get_attachment_url($image_left) . '">';
       } else {
         $img_elem = wp_get_attachment_image($image_left, 'item-l-6-4x3');
       }
 ?>
           <div class="front-image-holder desktop-front-item">
-            <a href="<?php echo get_the_permalink($project['id']); ?>" class="project-<?php echo $project['id']; ?>  grid-column justify-center align-items-center">
+            <a href="<?php echo get_the_permalink($post->ID); ?>" class="project-<?php echo $post->ID; ?>  grid-column justify-center align-items-center">
               <?php echo $img_elem; ?>
             </a>
           </div>
-<?php 
+<?php
     }
 
     if (!empty($image_right)) {
       $check_filetype = wp_check_filetype(wp_get_attachment_url($image_right));
-      if ($check_filetype['ext'] == 'gif') { 
+      if ($check_filetype['ext'] == 'gif') {
         $img_elem = '<img src="' . wp_get_attachment_url($image_right) . '">';
       } else {
         $img_elem = wp_get_attachment_image($image_right, 'item-l-6-4x3');
       }
 ?>
           <div class="front-image-holder mobile-front-item">
-            <a href="<?php echo get_the_permalink($project['id']); ?>" class="project-<?php echo $project['id']; ?>  grid-column justify-center align-items-center">
+            <a href="<?php echo get_the_permalink($post->ID); ?>" class="project-<?php echo $post->ID; ?>  grid-column justify-center align-items-center">
               <?php echo $img_elem; ?>
             </a>
           </div>
-<?php 
+<?php
     }
   }
 ?>
@@ -63,27 +79,29 @@ if ($projects) {
       <div class="grid-item item-s-12 item-l-6">
         <div class="scroll-col scroll-col-right text-align-center desktop-front-item" data-side="right">
 <?php
-  $projects_reversed = array_reverse($projects);
+  $posts_reversed = array_reverse($query->posts);
+  $query->posts = $posts_reversed;
 
-  foreach ($projects_reversed as $project) {
+  while ( $query->have_posts() ) {
+    $query->the_post();
 
-    $image_right = get_post_meta($project['id'], '_igv_front_image_right_id', true);
+    $image_right = get_post_meta($post->ID, '_igv_front_image_right_id', true);
 
     if (!empty($image_right)) {
       $check_filetype = wp_check_filetype(wp_get_attachment_url($image_right));
-      if ($check_filetype['ext'] == 'gif') { 
+      if ($check_filetype['ext'] == 'gif') {
         $img_elem = '<img src="' . wp_get_attachment_url($image_right) . '">';
       } else {
         $img_elem = wp_get_attachment_image($image_right, 'item-l-6-4x3');
       }
 ?>
-          
+
           <div class="front-image-holder desktop-front-item">
-            <a href="<?php echo get_the_permalink($project['id']); ?>" class="project-<?php echo $project['id']; ?>  grid-column justify-center align-items-center">
+            <a href="<?php echo get_the_permalink($post->ID); ?>" class="project-<?php echo $post->ID; ?>  grid-column justify-center align-items-center">
               <?php echo $img_elem; ?>
             </a>
           </div>
-<?php 
+<?php
     }
   }
 ?>
